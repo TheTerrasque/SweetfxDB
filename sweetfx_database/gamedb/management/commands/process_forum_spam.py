c9185484ca7553cb7d87311228794cbb67fc5608
Mikael Turøy <mikael.turoy@holbergeeg.org>
2025-02-15 23:20:50 +0100

    bugfix and custom endpoint params

--- sweetfx_database/gamedb/management/commands/process_forum_spam.py
+++ sweetfx_database/gamedb/management/commands/process_forum_spam.py
@@ -1,7 +1,16 @@
 from django.core.management.base import BaseCommand, CommandError
 from sweetfx_database.forum.models import ForumPost
-from sweetfx_database.gamedb.openai_spamjudge import judge_post
+from sweetfx_database.gamedb.openai_spamjudge import OpenAISpamJudge, BASE_URL, MODEL, KEY
 from tqdm import tqdm
+
+from termcolor import colored
+
+TYPES = {
+    2: colored("SPAM", "light_red"),
+    1: colored("OK", "light_green"),
+    3: colored("Unsure", "light_yellow"),
+    4: colored("ERROR", "light_red", "on_yellow", attrs=["bold"]),
+}
 
 class Command(BaseCommand):
     help = 'Filter forum spam entries'
@@ -15,6 +24,24 @@ class Command(BaseCommand):
         )
 
         parser.add_argument(
+            '--baseurl',
+            default=BASE_URL,
+            help="OpenAI Base URL"
+        )
+
+        parser.add_argument(
+            '--key',
+            default=KEY,
+            help="OpenAI Key"
+        )
+
+        parser.add_argument(
+            '--model',
+            default=MODEL,
+            help="OpenAI model"
+        )
+
+        parser.add_argument(
             '--quiet',
             action='store_true',
             default=False,
@@ -23,6 +50,7 @@ class Command(BaseCommand):
 
         # Add an option for the number of posts to process (default: 10)
         parser.add_argument(
+            "-n",
             '--num-posts',
             type=int,
             default=10,
@@ -30,15 +58,25 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
+        oai = OpenAISpamJudge(
+            options.get("baseurl"),
+            options.get("key"),
+            options.get("model")
+        )
+
         progress = options.get('progress')
         print_info = not options.get('quiet')
         query = ForumPost.objects.filter(state = 0)[:options.get('num_posts')]
         if progress:
             query = tqdm(query)
         for post in query:
-            print_info and tqdm.write(f"Processing message { post.id } by { post.creator }")
-            r = judge_post(post) 
-            if r and print_info:
-                tqdm.write(" Post judged: %s" % post.id)
-                if r >= 2:
-                    tqdm.write(" User %s lost post privs" % post.creator)
+
+            
+            r = oai.judge_post(post) 
+
+            url = colored("https://sfx.thelazy.net" + post.thread.get_absolute_url(), "dark_grey")
+            text = f"[{ post.id }] by { post.creator }\t{TYPES[post.state]}\t{ url }"
+
+            if r >=2:
+                text += "\tUser lost posting rights"
+            print_info and tqdm.write(text)
--- sweetfx_database/gamedb/openai_spamjudge.py
+++ sweetfx_database/gamedb/openai_spamjudge.py
@@ -15,45 +15,50 @@ RULES = getattr(settings, "FORUM_RULES", None)
 POST_PROMPT = getattr(settings, "DEFAULT_POST_PROMPT", None)
 THREAD_PROMPT = getattr(settings, "DEFAULT_THREAD_PROMPT", None)
 
-active = False
-
 RESPONSE_FORMAT = """ Give the answer in json: { "wanted": "yes|no|maybe", "reason":"<reason>"}"""
 
-if KEY and MODEL and RULES and POST_PROMPT and THREAD_PROMPT:
-    client = openai.OpenAI(api_key=KEY, base_url=BASE_URL)
-    post_template = jinja2.Template(POST_PROMPT + RESPONSE_FORMAT)
-    thread_template = jinja2.Template(THREAD_PROMPT + RESPONSE_FORMAT)
-    active = True
+class OpenAISpamJudge:
+    permissions_to_remove = ["post_on_forum", "post_on_games"]
+    active = False
+    def __init__(self, base_url = BASE_URL, key=KEY, model=MODEL, args=ARGS, rules=RULES, post_prompt=POST_PROMPT, thread_prompt = THREAD_PROMPT):
+        if key and model and rules and post_prompt and thread_prompt:
+            self.client = openai.OpenAI(api_key=key, base_url=base_url)
+            self.post_template = jinja2.Template(post_prompt + RESPONSE_FORMAT)
+            self.thread_template = jinja2.Template(thread_prompt + RESPONSE_FORMAT)
+            self.rules = rules
+            self.model = model
+            self.args = args
+            self.active = True
 
-def remove_user_posting_permissions(user: fm.userdb.User):
-    forum_permission = fm.Permission.objects.get(codename="post_on_forum")
-    gamedb_permission = fm.Permission.objects.get(codename="post_on_games")
-    user.user_permissions.remove(forum_permission)
-    user.user_permissions.remove(gamedb_permission)
+    def remove_user_posting_permissions(self, user: fm.userdb.User):
+        for entry in self.permissions_to_remove:
+            permission = fm.Permission.objects.get(codename=entry)
+            user.user_permissions.remove(permission)
 
+    def call_llm_and_get_response(self, prompt):
+        r = self.client.chat.completions.create(model=self.model, messages=[{"role":"user", "content":prompt}], **self.args)
+        result = r.choices[0].message.content.strip()
+        # Clean markdown
+        result = re.sub(r"```[a-zA-Z]*", "", result).strip("`\n")
+        j = json.loads(result)
+        j["rating"] = {"yes": 1, "no": 2, "maybe": 3}[j["wanted"]]
+        return j
 
-def call_llm_and_get_response(prompt):
-    r = client.chat.completions.create(model=MODEL, messages=[{"role":"user", "content":prompt}], **ARGS)
-    result = r.choices[0].message.content.strip()
-    # Clean markdown
-    result = re.sub(r"```[a-zA-Z]*", "", result).strip("`\n")
-    j = json.loads(result)
-    j["rating"] = {"yes": 1, "no": 2, "maybe": 3}[j["wanted"]]
-    return j
+    def judge_post(self, post: fm.ForumPost):
+        if not self.active: return -1
 
-def judge_post(post: fm.ForumPost):
-    if not active: return
+        prompt = self.post_template.render(RULES=self.rules, post=post)
+        try:
+            response = self.call_llm_and_get_response(prompt)
+            post.update_state(response["rating"], response["reason"])
+        except Exception as e:
+            post.update_state(4, str(e))
 
-    prompt = post_template.render(RULES=RULES, post=post)
-    try:
-        response = call_llm_and_get_response(prompt)
-        post.update_state(response["rating"], response["reason"])
-    except Exception as e:
-        post.update_state(4, str(e))
-    if post.state in fm.POST_SPAM_STATES:
-        num_spams = fm.ForumPost.objects.filter(state__in=fm.POST_SPAM_STATES, creator=post.creator).count()
-        num_nonspams = fm.ForumPost.objects.filter(state__in=fm.POSTS_VISIBLE_STATES, creator=post.creator).count()
-        if num_spams > num_nonspams:
-            remove_user_posting_permissions(post.creator)
-            return 2
-        return 1
+        if post.state in fm.POST_SPAM_STATES:
+            num_spams = fm.ForumPost.objects.filter(state__in=fm.POST_SPAM_STATES, creator=post.creator).count()
+            num_nonspams = fm.ForumPost.objects.filter(state__in=fm.POSTS_VISIBLE_STATES, creator=post.creator).count()
+            if num_spams > num_nonspams:
+                self.remove_user_posting_permissions(post.creator)
+                return 2
+            return 1
+        return 0
